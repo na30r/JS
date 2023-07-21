aea81e298ef414f2c2143fbc794dde349a51b578
nasir moghtaderi <52619226+na30r@users.noreply.github.com>
2023-07-22 00:48:51 +0330

    add table and api

--- index.js
+++ index.js
@@ -9,10 +9,49 @@ root.appendChild(p);
 
 var users = [];
 
-getUsers("")
-    .then(a => (JSON.parse(a)))
-    .then(a => a.map(b => new User(b)))
-    .then(a => console.log(a))
-    .then(users = a)
-    .then(console.log(users))
+var tableHeaders = ["id", 'name', 'email', 'phone']
+
+function tableInit() {
+    var mainTable = document.getElementById('my-table');
+    var tableRow = document.createElement('tr')
+    tableHeaders.map(function (a) {
+        var cell = document.createElement('td')
+        cell.innerHTML = a
+        tableRow.appendChild(cell)
+    })
+    mainTable.appendChild(tableRow)
+}
+
+function tableData() {
+    getUsers("")
+        .then(a => (JSON.parse(a)))
+        .then(a => a.map(b => new User(b)))
+        .then(a => a.forEach(b => addRow(b)))
+}
+
+function addRow(user) {
+    var mainTable = document.getElementById('my-table');
+    var tableRow = document.createElement('tr')
+    tableRow.id = user['id']
+    tableHeaders.map(function (a) {
+        var cell = document.createElement('td')
+        cell.innerHTML = user[a]
+
+        tableRow.appendChild(cell)
+    })
+    var cell = document.createElement('td')
+    cell.innerHTML = 'X'
+    cell.id = user.id;
+    cell.className = 'deleteUser';
+    cell.addEventListener("click", function () {
+        deleteCell(id)
+    })
+    tableRow.appendChild(cell)
+    mainTable.appendChild(tableRow)
+}
+
+
+tableInit()
+tableData()
+
 
